423c3dc330e2c4dd904ead28798df21b11bd332f
Claudio Lins <claudio.lins@me.com>
2022-10-22 22:41:18 +0100

    ignoreBuildErrors: true,
    typescript: {
        // !! WARN !!
        // Dangerously allow production builds to successfully complete even if
        // your project has type errors.
        // !! WARN !!
        ignoreBuildErrors: true,
      },

--- src/components/CartButton/styles.ts
+++ src/components/CartButton/styles.ts
@@ -1,25 +1,25 @@
-import { styled } from "../../styles";
+import { styled } from "../../styles"
 
 export const CartButtonContainer = styled("button", {
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  border: 'none',
+  border: "none",
   borderRadius: 6,
   position: "relative",
 
-  '&:disabled': {
-    cursor: 'not-allowed',
-    opacity: 0.6
+  "&:disabled": {
+    cursor: "not-allowed",
+    opacity: 0.6,
   },
 
   backgroundColor: "$gray800",
   color: "$gray500",
 
-  width: '3rem',
-  height: '3rem',
+  width: "3rem",
+  height: "3rem",
 
   svg: {
     fontSize: 24,
-  }
+  },
 })
