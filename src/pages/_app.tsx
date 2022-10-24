ddee5990ce6e1cdf5505eba03ed7ab3351f1686a
Claudio Lins <claudio.lins@me.com>
2022-10-24 17:56:11 +0100

    CartContextProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import type { AppProps } from "next/app"
 import { Header } from "../components/Header"
+import { CartContextProvider } from "../context/CartContaxt"
 import { globalStyles } from "../styles/global"
 import { Container } from "../styles/pages/app"
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header />
-      <Component {...pageProps} />
-    </Container>
+    <CartContextProvider>
+      <Container>
+        <Header />
+        <Component {...pageProps} />
+      </Container>
+    </CartContextProvider>
   )
 }
